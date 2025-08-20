0d614c91a51425f9fdd7fb7b443a4433c3847ab9
Thiago Pucinelli <154468630+TheThiagoPucinelli@users.noreply.github.com>
2025-08-19 23:47:29 -0300

    Update chave_lista.php

--- Pages/chave_lista.php
+++ Pages/chave_lista.php
@@ -10,7 +10,7 @@
 
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="../IMG/cmpage.png">
+    <link rel="icon" type="image/svg+xml" href="../IMG/logo.svg">
 
     <!-- Tailwind CSS via CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
@@ -74,3 +74,4 @@
 
 </body>
 </html>
+
